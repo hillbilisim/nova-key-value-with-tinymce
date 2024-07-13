05e92a8d0785188a2f3bdf58a43be3563f3dc0be
Emre Sarıoğlu <emresarioglu89@gmail.com>
2024-07-13 03:01:32 +0300

    v1.0.13

--- resources/js/components/FormField.vue
+++ resources/js/components/FormField.vue
@@ -89,7 +89,7 @@ export default {
       });
     }
   },
-  data: () => ({theData: []}),
+  data: () => ({theData: [a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p, q, r, s, t, u, v, w, x, y, z]}),
 
   created() {
     this.theData = map(
