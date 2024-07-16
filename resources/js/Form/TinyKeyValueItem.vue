24df432b8d49f3c57b7e4f7dc1c398643fbe7205
Emre Sarıoğlu <emresarioglu89@gmail.com>
2024-07-17 02:24:02 +0300

    v1.1.4

--- resources/js/Form/TinyKeyValueItem.vue
+++ resources/js/Form/TinyKeyValueItem.vue
@@ -97,29 +97,66 @@ export default {
         handleKeyFieldFocus() {
             this.$refs.keyField.select()
         },
-        options() {
-            let options = this.field.options
+      /*
+        * Set the initial, internal value for the field.
+        */
+      setInitialValue() {
+        this.value = this.field.value || ''
+      },
 
-            if (options.use_lfm) {
-                options['file_picker_callback'] = this.filePicker
-            }
+      /**
+       * Fill the given FormData object with the field's internal value.
+       */
+      fill(formData) {
+        formData.append(this.field.attribute, this.value || '')
+      },
 
-            if (options.use_dark && options.content_css_dark && options.skin_url_dark) {
-                if (document.documentElement.classList.contains('dark')) {
-                    options['content_css'] = options['content_css_dark']
-                    options['skin_url'] = options['skin_url_dark']
-                }
-            }
+      /**
+       * Update the field's internal value.
+       */
+      handleChange(value) {
+        this.value = value
+      },
 
-            return options
-        },
+      filePicker: function (callback, value, meta) {
+        let x = window.innerWidth || document.documentElement.clientWidth || document.getElementsByTagName('body')[0].clientWidth;
+        let y = window.innerHeight|| document.documentElement.clientHeight|| document.getElementsByTagName('body')[0].clientHeight;
+
+        let type = 'image' === meta.filetype ? 'Images' : 'Files';
+        let url  = this.options.path_absolute + this.options.lfm_url + '?editor=tinymce5&type=' + type;
+
+        tinymce.activeEditor.windowManager.openUrl({
+          url : url,
+          title : 'File manager',
+          width : x * 0.8,
+          height : y * 0.8,
+          onMessage: (api, message) => {
+            callback(message.content);
+          }
+        });
+      },
         handleValueFieldFocus() {
             this.$refs.valueField.select()
         },
     },
 
     computed: {
+      options() {
+        let options = this.field.options
 
+        if (options.use_lfm) {
+          options['file_picker_callback'] = this.filePicker
+        }
+
+        if (options.use_dark && options.content_css_dark && options.skin_url_dark) {
+          if (document.documentElement.classList.contains('dark')) {
+            options['content_css'] = options['content_css_dark']
+            options['skin_url'] = options['skin_url_dark']
+          }
+        }
+
+        return options
+      },
         isNotObject() {
             return !(this.item.value instanceof Object)
         },
